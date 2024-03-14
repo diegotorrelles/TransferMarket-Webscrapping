44caa09edc23454706d65b751189c8b43268d60e
Diego Torrelles <diegotorrelles@gmail.com>
2024-03-14 09:31:18 +0100

    funciona un solo jugador

--- Programming_uni.py
+++ Programming_uni.py
@@ -91,143 +91,226 @@ valoresBarra = barra.find_elements(By.CSS_SELECTOR,"li")
 elemento = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "//span[starts-with(@id, 'ASTAGQ_plc_close_')]")))     ### Para quitar la publi
 elemento.click()
 
+
+def valoresEquipos():
+    valoresDeMercadoSpan = valoresBarra[2].find_element(By.CSS_SELECTOR,"span").click()         ### Click de Valores de Mercado
+
+    elemento = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "a.main-navbar__dropdown-link[href='/primera-division/marktwerte/wettbewerb/ES1']")))
+    elemento.click()
+
+
+    shadow_host = driver.find_element(By.CSS_SELECTOR, 'tm-subnavigation[controller="wettbewerb"][id="ES1"][season="2023"][section="wettbewerb"][style="display: block; margin: 0 5px;"')
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+    element_inside_shadow_dom = shadow_root.find_element(By.CSS_SELECTOR, 'div').find_element(By.CSS_SELECTOR,"ul")
+
+    todosLi = element_inside_shadow_dom.find_elements(By.CSS_SELECTOR,"li.svelte-e7ru94.arrow")
+
+    #MIRAR SI SE PUEDE BORRAR
+    x=0  
+    while x <2:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+        x = x +1
+
+    liSolo = todosLi[2]
+    clicValoresMercado = liSolo.find_element(By.CSS_SELECTOR,"a")
+
+
+    clicValoresMercado.click()
+
+    vistaGeneral = liSolo.find_element(By.CSS_SELECTOR,"dd").find_elements(By.CSS_SELECTOR,"li")
+    elementoValoresClubes = vistaGeneral[1].find_element(By.CSS_SELECTOR,"a")
+    elementoValoresClubes.click()
+
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    #Estos son todas las casillas donde estan los nombres,fecha y valor de cada club 
+    #elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
+    
+    #print("-------------------------------------------------------")
+
+    fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
+    driver.execute_script("arguments[0].scrollIntoView();",fijador)
+
+    fechas_generadas = generar_fechas()
+
+    time.sleep(1)
+
+    #print("\n\n")  
+    #print("-----------------------------------")
+
+
+    #Estos son todas las casillas donde estan los nombres,fecha y valor de cada club 
+    elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
+    #print(elementos[0].get_attribute("outerHTML")) 
+    #print("\n\n")  
+    #print("-----------------------------------")
+
+    fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
+    driver.execute_script("arguments[0].scrollIntoView();",fijador)
+
+
+    time.sleep(1)
+
+    df = pd.DataFrame(columns=['Nombre Equipo', 'Valor Equipo (en millones $)', 'Fecha'])
+
+    #'''
+    contadorDF = 0
+    iter = 0
+    i=0
+    while i < len(fechas_generadas):
+        #print(f"ENTRANDO EN LA ITERACION NUMERO {i}")
+
+        #fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
+        #driver.execute_script("arguments[0].scrollIntoView();",fijador)
+
+        try:
+            time.sleep(1)
+            #barraFecha = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "div.inline-select div")))  # Combine selectors directly
+            barraFecha = driver.find_element(By.CSS_SELECTOR,"div.inline-select").find_element(By.CSS_SELECTOR,"div")
+            #print(barraFecha.get_attribute("outerHTML"))      
+            barraFecha.click()
+            #time.sleep(20)
+            #barraEscribir = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "input[type='search'][autocomplete='off'][tabindex='0']")))
+            barraEscribir = driver.find_element(By.CSS_SELECTOR,"input[type='search'][autocomplete='off'][tabindex='0']")
+        
+            #print(f"esta es la fecha que tiene que escribir {fechas_generadas[i]}")
+        
+            barraEscribir.send_keys(fechas_generadas[i].replace('"', ''))
+            barraEscribir.send_keys(Keys.ENTER)
+        
+        except:
+            #print("me fui por la execption")
+            time.sleep(1)
+            continue
+
+        #time.sleep(1)
+        botonMostar = driver.find_element(By.CSS_SELECTOR,"input[type='submit'].right.small.button")
+        botonMostar.click()
+        x = 0
+        #wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "td.hauptlink a")))
+        #print(elementos[x].find_element(By.CSS_SELECTOR,"td.hauptlink a").text)
+        elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
+        
+        
+        while x<=2:
+            wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "tr td.hauptlink a")))
+            
+            nombre = elementos[x].find_element(By.CSS_SELECTOR,"tr td.hauptlink a").text
+            valor = elementos[x].find_element(By.CSS_SELECTOR,"tr td.rechts a").text
+
+            #print(f"este equipo:{nombre} tiene este valor {valor}")
+            
+            if "mil mill. €" in valor:
+                mult = 1000000000
+                
+            else:
+                mult = 1000000
+                
+
+            valor_sin_formato = valor.split(" ")[0].replace(",", "")  # Extract before space, remove comma
+            valor_entero = int(valor_sin_formato) * mult
+
+            
+
+            df.loc[contadorDF] = [nombre,
+                        valor_entero,
+                        fechas_generadas[i].replace('"','')]     
+            contadorDF = contadorDF + 1
+            
+            x = x +1
+        #print(f"SALIENDO DE LA ITERACION NUMERO {i}")
+        i = i + 24
+        #24 es un año
+        df.to_csv("valoresEquipo.csv", index=False, sep=',')
+
+        #print(elemento.get_attribute("outerHTML"))   
+        #print(nombreEquipo.get_attribute("outerHTML"))  
+    #'''
+    #print("---------------------------")
+    #print("\n\n")
+
+    print(df)
+
+#time.sleep(1)
 valoresDeMercadoSpan = valoresBarra[2].find_element(By.CSS_SELECTOR,"span").click()         ### Click de Valores de Mercado
 
 elemento = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "a.main-navbar__dropdown-link[href='/primera-division/marktwerte/wettbewerb/ES1']")))
 elemento.click()
 
-
+'''
 shadow_host = driver.find_element(By.CSS_SELECTOR, 'tm-subnavigation[controller="wettbewerb"][id="ES1"][season="2023"][section="wettbewerb"][style="display: block; margin: 0 5px;"')
 shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
 element_inside_shadow_dom = shadow_root.find_element(By.CSS_SELECTOR, 'div').find_element(By.CSS_SELECTOR,"ul")
+'''
 
-todosLi = element_inside_shadow_dom.find_elements(By.CSS_SELECTOR,"li.svelte-e7ru94.arrow")
-
-#MIRAR SI SE PUEDE BORRAR
-x=0  
-while x <2:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
-    x = x +1
-
-liSolo = todosLi[2]
-clicValoresMercado = liSolo.find_element(By.CSS_SELECTOR,"a")
-
-
-clicValoresMercado.click()
-
-vistaGeneral = liSolo.find_element(By.CSS_SELECTOR,"dd").find_elements(By.CSS_SELECTOR,"li")
-elementoValoresClubes = vistaGeneral[1].find_element(By.CSS_SELECTOR,"a")
-elementoValoresClubes.click()
-
-#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-#Estos son todas las casillas donde estan los nombres,fecha y valor de cada club 
-#elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
-  
-#print("-------------------------------------------------------")
 
 fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
 driver.execute_script("arguments[0].scrollIntoView();",fijador)
 
-fechas_generadas = generar_fechas()
+tablaJugadores = driver.find_element(By.CSS_SELECTOR,"div.responsive-table").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
 
-time.sleep(1)
+class JugadorFutbol:
+    def __init__(self, nombreCompleto: str, valorMercado: int, club: str):
+        self.nombreCompleto = nombreCompleto
+        self.valorMercado = valorMercado
+        self.club = club
 
-#print("\n\n")  
-#print("-----------------------------------")
+    def __str__(self):
+        return f"Nombre: {self.nombreCompleto}\nValor de mercado: {self.valorMercado}\nClub: {self.club}"
+
+jugadoresMadrid = 0
+jugadoresBarca = 0
+jugadoresReal = 0
+suma = 0  
+x = 0
+
+futbolistas = []
+while suma != 9 and x != 10:
 
 
-#Estos son todas las casillas donde estan los nombres,fecha y valor de cada club 
-elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
-#print(elementos[0].get_attribute("outerHTML")) 
-#print("\n\n")  
-#print("-----------------------------------")
+    soccerPlayer = tablaJugadores[x].find_elements(By.CSS_SELECTOR,"td")
 
-fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
-driver.execute_script("arguments[0].scrollIntoView();",fijador)
+    print(soccerPlayer[1].get_attribute("outerHTML"))  
 
+    equipo = soccerPlayer[7].find_element(By.CSS_SELECTOR,"a").get_attribute("title") 
 
-time.sleep(1)
+    if equipo == "Real Madrid CF" and jugadoresMadrid == 3:
+        pass
 
-df = pd.DataFrame(columns=['Nombre Equipo', 'Valor Equipo (en millones $)', 'Fecha'])
+    elif equipo == "FC Barcelona" and jugadoresBarca == 3:
+        pass
 
-#'''
-contadorDF = 0
-iter = 0
-i=0
-while i < len(fechas_generadas):
-    #print(f"ENTRANDO EN LA ITERACION NUMERO {i}")
+    elif equipo == "Real Sociedad" and jugadoresReal == 3:
+        pass
+    else:
 
-    #fijador = driver.find_element(By.CSS_SELECTOR, "h1.content-box-headline")
-    #driver.execute_script("arguments[0].scrollIntoView();",fijador)
-
-    try:
-        time.sleep(1)
-        #barraFecha = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "div.inline-select div")))  # Combine selectors directly
-        barraFecha = driver.find_element(By.CSS_SELECTOR,"div.inline-select").find_element(By.CSS_SELECTOR,"div")
-        #print(barraFecha.get_attribute("outerHTML"))      
-        barraFecha.click()
-        #time.sleep(20)
-        #barraEscribir = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "input[type='search'][autocomplete='off'][tabindex='0']")))
-        barraEscribir = driver.find_element(By.CSS_SELECTOR,"input[type='search'][autocomplete='off'][tabindex='0']")
-    
-        #print(f"esta es la fecha que tiene que escribir {fechas_generadas[i]}")
-    
-        barraEscribir.send_keys(fechas_generadas[i].replace('"', ''))
-        barraEscribir.send_keys(Keys.ENTER)
-      
-    except:
-        #print("me fui por la execption")
-        time.sleep(1)
-        continue
-
-    #time.sleep(1)
-    botonMostar = driver.find_element(By.CSS_SELECTOR,"input[type='submit'].right.small.button")
-    botonMostar.click()
-    x = 0
-    #wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "td.hauptlink a")))
-    #print(elementos[x].find_element(By.CSS_SELECTOR,"td.hauptlink a").text)
-    elementos = driver.find_element(By.CSS_SELECTOR,"table.items").find_element(By.CSS_SELECTOR,"tbody").find_elements(By.CSS_SELECTOR,"tr")
-    
-    
-    while x<=2:
-        wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, "tr td.hauptlink a")))
+        nombre = soccerPlayer[1].find_element(By.CSS_SELECTOR,"tr").find_elements(By.CSS_SELECTOR,"td")[1].find_element(By.CSS_SELECTOR,"a").text
         
-        nombre = elementos[x].find_element(By.CSS_SELECTOR,"tr td.hauptlink a").text
-        valor = elementos[x].find_element(By.CSS_SELECTOR,"tr td.rechts a").text
+        valorJugador = soccerPlayer[8].find_element(By.CSS_SELECTOR,"a").text
 
-        #print(f"este equipo:{nombre} tiene este valor {valor}")
+        valor_sin_formato = valorJugador.split(" ")[0].replace(",", "")  # Extract before space, remove comma
+        valor_entero = int(valor_sin_formato) * 1000000
+
+        futbolista = JugadorFutbol(nombre,valor_entero,equipo)
+        futbolistas.append(futbolista)
+
+        if equipo == "Real Madrid CF" and jugadoresMadrid == 3:
+            jugadoresMadrid = jugadoresMadrid +1
+
+        elif equipo == "FC Barcelona" and jugadoresBarca == 3:
+            jugadoresBarca = jugadoresBarca + 1
+
+        elif equipo == "Real Sociedad" :
+            jugadoresReal = jugadoresReal + 1
+
+        suma = jugadoresMadrid + jugadoresBarca + jugadoresReal
+
+        x = x +1 
+#print(equipo.get_attribute("outerHTML"))  
         
-        if "mil mill. €" in valor:
-            mult = 1000000000
-            
-        else:
-            mult = 1000000
-            
+print(futbolistas)
 
-        valor_sin_formato = valor.split(" ")[0].replace(",", "")  # Extract before space, remove comma
-        valor_entero = int(valor_sin_formato) * mult
-
-        
-
-        df.loc[contadorDF] = [nombre,
-                    valor_entero,
-                    fechas_generadas[i].replace('"','')]     
-        contadorDF = contadorDF + 1
-        
-        x = x +1
-    #print(f"SALIENDO DE LA ITERACION NUMERO {i}")
-    i = i + 24
-    #24 es un año
-    df.to_csv("valoresEquipo.csv", index=False, sep=',')
-
-    #print(elemento.get_attribute("outerHTML"))   
-    #print(nombreEquipo.get_attribute("outerHTML"))  
-#'''
-#print("---------------------------")
-#print("\n\n")
-
-print(df)
+print("\n\n")
 
 input("pulse para terminar")    
